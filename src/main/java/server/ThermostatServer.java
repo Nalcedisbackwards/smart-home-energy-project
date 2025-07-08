04c4504f500988d18601d544229d17a5a9712341
Declan Hughes <x24215651@student.ncirl.ie>
2025-07-08 20:00:09 +0100

    Created ThermostatServer.java
    Contains a hopefully working setTargetTemperature and placeholders for
    the other methods

--- src/main/java/server/ThermostatServer.java
+++ src/main/java/server/ThermostatServer.java
@@ -1,24 +1,26 @@
 package server;
 
 import java.io.IOException;
-import java.util.Iterator;
+import thermostat.protos.SmartThermostatGrpc.SmartThermostatImplBase;
+import thermostat.protos.SetTargetTemperatureRequest;
+import thermostat.protos.SetTargetTemperatureResponse;
+import thermostat.protos.GetTemperatureHistoryRequest;
+import thermostat.protos.TemperatureReading;
+import thermostat.protos.GetAverageTemperatureResponse;
 import java.net.InetAddress;
 import java.util.logging.Logger;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
-import io.grpc.Status;
-import io.grpc.StatusRuntimeException;
 import io.grpc.stub.StreamObserver;
 import javax.jmdns.JmDNS;
 import javax.jmdns.ServiceInfo;
 
 
-
-public class ThermostatServer extends ThermostatServiceImpl{
+public class ThermostatServer extends SmartThermostatImplBase {
 	
+	private volatile double currentTargetTemp = 20.0;
 	
 	private static final Logger logger = Logger.getLogger(ThermostatServer.class.getName());
-	
 	
 	public static void main(String[] args) throws IOException, InterruptedException {
 		
@@ -43,6 +45,43 @@ public class ThermostatServer extends ThermostatServiceImpl{
 		
 		server.awaitTermination();
 		
+	}
+	
+	@Override
+	 public void setTargetTemperature(SetTargetTemperatureRequest req, StreamObserver<SetTargetTemperatureResponse> responseObserver) {
+		double newTemp = req.getTargetTemp(); // Read the requested temperature
+		currentTargetTemp = newTemp;
+		
+		SetTargetTemperatureResponse reply = SetTargetTemperatureResponse.newBuilder()
+				.setSuccess(true)
+				.build();
+		responseObserver.onNext(reply);
+		responseObserver.onCompleted();
+	}
+	
+	@Override
+    public void streamTemperatureHistory(GetTemperatureHistoryRequest req, StreamObserver<TemperatureReading> responseObserver) {
+		responseObserver.onCompleted();
+	}
+	
+	@Override
+	 public StreamObserver<TemperatureReading> getAverageTemperature(StreamObserver<GetAverageTemperatureResponse> responseObserver) {
+		return new StreamObserver<TemperatureReading>() {
+			 @Override
+		     public void onNext(TemperatureReading reading) {
+		            
+		     }
+			 
+			 @Override
+			 public void onError(Throwable t) {
+		      
+			 }
+			 
+			 @Override
+		     public void onCompleted() {
+				 responseObserver.onCompleted();
+			 }
+		};
 	}
 	
 }
