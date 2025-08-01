8f8be814d9642d38018fddadec87047a94d5ab62
Declan Hughes <x24215651@student.ncirl.ie>
2025-08-01 12:00:49 +0100

    Added stream temperature to the thermostat server

--- src/main/java/server/ThermostatServer.java
+++ src/main/java/server/ThermostatServer.java
@@ -8,6 +8,7 @@ import thermostat.protos.GetTemperatureHistoryRequest;
 import thermostat.protos.TemperatureReading;
 import thermostat.protos.GetAverageTemperatureResponse;
 import java.net.InetAddress;
+import java.util.Random;
 import java.util.logging.Logger;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
@@ -15,18 +16,29 @@ import io.grpc.stub.StreamObserver;
 import javax.jmdns.JmDNS;
 import javax.jmdns.ServiceInfo;
 
+/*
+ * ThermostatServer implements the SmartThermostat gRPC services defined in the proto file. 
+ */
 
 public class ThermostatServer extends SmartThermostatImplBase {
 	
+	//Initial target temp to be set upon starting
 	private volatile double currentTargetTemp = 20.0;
 	
+	
+	//
 	private static final Logger logger = Logger.getLogger(ThermostatServer.class.getName());
 	
+	//Random genrator to simulate noise in temperature readings
+	private final Random random = new Random();
+	
+	
+	//Main method to start the gRPC server and register service via jmDNS discovery
 	public static void main(String[] args) throws IOException, InterruptedException {
-		
 		ThermostatServer thermostatserver = new ThermostatServer();
+		int port = 50051;//Network port for gRPC service
 		
-		int port = 50051;
+		//Build and start the gRPC server
 		Server server = ServerBuilder
 				.forPort(port)
 				.addService(thermostatserver)
@@ -36,22 +48,25 @@ public class ThermostatServer extends SmartThermostatImplBase {
 		logger.info("Thermostat server started, listening on " + port);
 		
 		
-		
+		//jmDNS registration on advertise the service on the local network
 		JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());
 		
 		ServiceInfo serviceInfo = ServiceInfo.create("_thermostat._grpc._tcp.local.", "ThermostatService", port, "Thermostat Server will give you the current temperature");
 		jmdns.registerService(serviceInfo);
 		System.out.println("Starting the Thermostat Server loop");
 		
-		server.awaitTermination();
+		server.awaitTermination();//keep server running
 		
 	}
 	
+	
+	//Unary RPC: Sets new target temp
 	@Override
 	 public void setTargetTemperature(SetTargetTemperatureRequest req, StreamObserver<SetTargetTemperatureResponse> responseObserver) {
 		double newTemp = req.getTargetTemp(); // Read the requested temperature
 		currentTargetTemp = newTemp;
 		
+		//build and send a success response
 		SetTargetTemperatureResponse reply = SetTargetTemperatureResponse.newBuilder()
 				.setSuccess(true)
 				.build();
@@ -59,8 +74,26 @@ public class ThermostatServer extends SmartThermostatImplBase {
 		responseObserver.onCompleted();
 	}
 	
+	//Server streaming RPC: Streams simulated temperature readings between two timestamps
 	@Override
     public void streamTemperatureHistory(GetTemperatureHistoryRequest req, StreamObserver<TemperatureReading> responseObserver) {
+		long startTH = req.getStartTimestamp(); //start time in ms
+		long endTH = req.getEndTimestamp(); //end time in ms
+		long stepTH = 1_000; //interval between samples = 1 sec
+		
+		//loop from start to end timestamp sending 1 reading per sec
+		for (long i = startTH; i <= endTH; i += stepTH) {
+			//simulate temp with noise around current target
+			double noisyTemp = currentTargetTemp + random.nextGaussian()*0.5;
+			
+			//build TemperatureReading message
+			TemperatureReading reading = TemperatureReading.newBuilder()
+					.setTemperature(noisyTemp)
+					.setTimestamp(i)
+					.build();
+			//send each reading to the client
+			responseObserver.onNext(reading);
+		}
 		responseObserver.onCompleted();
 	}
 	
